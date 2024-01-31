53da761b03d5425664ac5462264c98585e1db38c
Adithi M <adithimuralidhara@gmail.com>
2024-01-31 15:32:15 +0530

    update

---
+++ nutri/src/main.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./second.css";
+import Nutri from "./component/nutri";
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <React.StrictMode>
+    <Nutri></Nutri>
+  </React.StrictMode>
+);
+
+
+reportWebVitals();
